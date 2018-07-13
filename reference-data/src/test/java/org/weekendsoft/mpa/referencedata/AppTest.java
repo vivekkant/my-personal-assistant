690a49606cf543164e2de3d532fbfc76383616a7
vivekkant <vivek.kant@gmail.com>
2018-07-13 12:21:24 +0530

    Test case

--- reference-data/src/test/java/org/weekendsoft/mpa/referencedata/AppTest.java
+++ reference-data/src/test/java/org/weekendsoft/mpa/referencedata/AppTest.java
@@ -1,20 +1,16 @@
 package org.weekendsoft.mpa.referencedata;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.weekendsoft.mpa.referencedata.controller.HomeController;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+
+	@Test
+    public void testApp() {
+		HomeController hc = new HomeController();
+		String result = hc.home();
+		assertEquals(result, "Home URL default response of the reference data microservice");
     }
 }
